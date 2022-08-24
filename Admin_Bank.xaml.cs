950fb9b9d40fd182a0f801d0e824b9b5e8c9fc78
mosein <1380mirza@gmail.com>
2022-08-24 14:32:30 +0430

    admin bank acc xaml cs created

---
+++ Admin_Bank.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Admin_Bank.xaml
+    /// </summary>
+    public partial class Admin_Bank : Window
+    {
+        public Admin_Bank()
+        {
+            InitializeComponent();
+            balance_label.Content = File.ReadAllText("bankmoney.txt");
+
+        }
+
+
+        private void BANKENTER_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(BANKDONATION.Text) > 0)
+            {
+                mainpayment MP = new mainpayment();
+                MP.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("amount is not correct");
+            }
+
+
+        }
+    }
+}
